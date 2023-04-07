c9778f518426cec7b02c56f71e9349cdab78b90c
Esra Aydın <127955087+esraydin25@users.noreply.github.com>
2023-04-07 17:01:28 +0300

    07042023 update
    (cherry picked from commit 7ba0bcd9d867f8d9f0d961036438deb37ae0fdcc)

--- rentacar/src/main/java/kodlama/io/rentacar/business/dto/requests/update/UpdateMaintenanceRequest.java
+++ rentacar/src/main/java/kodlama/io/rentacar/business/dto/requests/update/UpdateMaintenanceRequest.java
@@ -5,14 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateMaintenanceRequest {
-    private int car_id;
-
+    private int carId;
     private String information;
-
-
+    private boolean isCompleted;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
 }
