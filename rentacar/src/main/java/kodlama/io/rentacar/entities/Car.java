2d22cd30adeaf33a99d23b01cb3e9d24360f9172
Esra Aydın <127955087+esraydin25@users.noreply.github.com>
2023-04-08 00:53:19 +0300

    revert deneme

--- rentacar/src/main/java/kodlama/io/rentacar/entities/Car.java
+++ rentacar/src/main/java/kodlama/io/rentacar/entities/Car.java
@@ -22,12 +22,6 @@ public class Car {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private int modelYear;
-
-
-
-
-
-    
     private String plate;
     @Enumerated(EnumType.STRING)
     private State state;
